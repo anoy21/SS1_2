2c3686833f4753a6353673d5c016b3aa63e77d7c
anoy21 <thienhung.kp2@gmail.com>
2023-05-14 12:01:58 +0700

    first

---
+++ app.js
@@ -0,0 +1,41 @@
+// for (var i = 10; i >= 0; i--) {
+//     document.write(i + "<br>");
+//     console.log(i);
+// }
+
+// for (var i = 1; i < 11; i++ ) {
+//     document.write(i + ". Hi World")
+//     console.log(i + ". Hi World")
+// }
+
+
+
+
+// for (var i =1; i <11; i++) {
+//     for (var j = 1; j < 11; j++ ) {
+//         var y = i * j
+//         console.log( i + "x" + j  + "="  + y )
+//         document.write( i + "x" + j + "=" + y + "<br>" ) 
+//     }
+// }
+
+// var i = 0
+// while (i < 11) {
+//     document.write(i + "<br>" );
+//     i++;
+// }
+
+
+
+// var i = 10
+// while (i >= 0) {
+//     document.write(i + "<br>" );
+//     i--;
+// }
+
+var i = 0
+
+do {document.write(i + "<br>");
+i++;
+}
+while (i < 11 )
